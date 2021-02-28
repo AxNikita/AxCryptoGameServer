535a3d7bece5ddbf087e6b5ba0ad042f528951a4
AxNikita <kotovnikita00744@gmail.com>
2021-02-28 15:49:25 +0600

    Initial commit

---
+++ src/main/java/com/axproject/axcrypto/AxcryptoApplication.java
@@ -0,0 +1,13 @@
+package com.axproject.axcrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxcryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxcryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axproject/axcrypto/AxcryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axproject.axcrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxcryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
